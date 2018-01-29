5d8380a3fb5090897b2ad953ac746108e117d5a8
Panu Kalliokoski <panu.kalliokoski@solita.fi>
2018-01-29 13:39:23 +0200

    Shorten code by eliminating common expressions, replacing zip+uncurry with zipWith, and eliminating superfluous casting to list and back

--- day3.hs
+++ day3.hs
@@ -1,30 +1,25 @@
 
-import Data.List (tails, concatMap, find)
+import Data.List (tails, find)
 import Data.Array (listArray, (!), elems)
 
 adjCellsForSide :: Int -> Int -> Int -> [[Int]]
 adjCellsForSide start length innerStart = firstAdj : restAdj where
- firstAdj = [innerStart, innerStart + 1, start - 2, start - 1]
- restAdj = zipWith (++) innerAdj thisRoundAdj
+ firstAdj = [start - 2, start - 1, innerStart, innerStart + 1]
+ restAdj = zipWith (:) [start .. start + length - 2] innerAdj
  innerAdj = map (take 3) $ tails [innerStart .. innerStart + length - 2]
- thisRoundAdj = map (:[]) [start .. start + length - 2]
 
 adjCellsForCircle :: Int -> Int -> Int -> [[Int]]
-adjCellsForCircle start len inner = side1 ++ side2 ++ side3 ++ side4 where
- side1 = side1Start ++ side1Rest
- side1Start = [[inner, start - 1], [inner, inner + 1, start - 1, start]]
- side1Rest = drop 2 $ adjCellsForSide start len (inner-1)
- side2 = adjCellsForSide (start + len) len (inner - 3 + len)
- side3 = adjCellsForSide (start + len*2) len (inner - 1 + (len-2)*2)
- side4 = init $ adjCellsForSide (start + len*3) (len+1) (inner - 1 + (len-2)*3)
-
-uncurry3 f (a, b, c) = f a b c
+adjCellsForCircle start len inner = side1Start ++ rest where
+ side1Start = [[start - 1, inner], [start - 1, start, inner, inner + 1]]
+ rest = init $ drop 2 $ concat $ zipWith makeSide [0..] [0,0,0,1]
+ makeSide side diff =
+  adjCellsForSide (start + len*side) (len+diff) (inner - 1 + (len-2)*side)
 
 adjCellsForSpiral :: [[Int]]
 adjCellsForSpiral = initSeq ++ outerCircles where
  initSeq = [[1], [1,2], [1,2,3], [1,4], [1,4,5], [1,6], [1,2,6,7], [1,2,8]]
- outerCircles = concatMap (uncurry3 adjCellsForCircle) circleParams
- circleParams = zip3 (tail circleStarts) [4,6..] circleStarts
+ outerCircles =
+  concat $ zipWith3 adjCellsForCircle (tail circleStarts) [4,6..] circleStarts
  circleStarts = map (1+) $ map (^2) [1,3..]
 
 memoryContents =
