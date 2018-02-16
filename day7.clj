f782f8537fc1b7068d121775617728ab66b1d52e
Panu Kalliokoski <panu.kalliokoski@solita.fi>
2018-02-16 03:18:52 +0200

    parameterise total-weight & friends with input

--- day7.clj
+++ day7.clj
@@ -14,27 +14,27 @@
         children (map #(get % 2) parsed)]
     [progs (zipmap progs weigths) (zipmap progs children)]))
 
-(let [[p w ch] (prop-maps (parse-input raw-input))]
-  (def programs p) (def weight w) (def children ch))
+(defn solve [programs weight children]
 
-(def total-weight
-  (memoize
-    (fn [prog]
-      (+ (weight prog)
-         (reduce + (map total-weight (children prog)))))))
+  (def total-weight
+    (memoize
+      (fn [prog]
+        (+ (weight prog)
+           (reduce + (map total-weight (children prog)))))))
 
-(defn balanced? [prog]
-  (->> (children prog) (map total-weight) set count (>= 1)))
+  (defn balanced? [prog]
+    (->> (children prog) (map total-weight) set count (>= 1)))
 
-(defn smallest-unbalanced [progs]
-  (->> progs (remove balanced?) (map #(vector (total-weight %) %))
-    sort first second))
+  (defn smallest-unbalanced [progs]
+    (->> progs (remove balanced?) (map #(vector (total-weight %) %))
+      sort first second))
 
-(let [parent (smallest-unbalanced programs)
-      ch (children parent)
-      [wrong-weight right-weight] (->> ch (map total-weight) frequencies
-                                    (map reverse) (map vec) sort (map second))
-      wrong-ch (first (filter #(= wrong-weight (total-weight %)) ch))
-      fixed-weight (+ (weight wrong-ch) right-weight (- wrong-weight))]
-  (println fixed-weight))
+  (let [parent (smallest-unbalanced programs)
+        ch (children parent)
+        [wrong-weight right-weight]
+        (->> ch (map total-weight) frequencies (map reverse) (map vec) sort (map second))
+        wrong-ch (first (filter #(= wrong-weight (total-weight %)) ch))]
+    (+ (weight wrong-ch) right-weight (- wrong-weight))))
+
+(println (apply solve (prop-maps (parse-input raw-input))))
 
