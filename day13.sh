2b1a0526dc1e368fa6b61791bbdf5356a9129919
Panu Kalliokoski <panu.kalliokoski@solita.fi>
2018-03-12 02:21:27 +0200

    Shell solutions for day 13

--- day13.sh
+++ day13.sh
@@ -1,1 +1,11 @@
-awk -F: '$1 % ($2 * 2 - 2) == 0 {sum += $1*$2} END {print sum}' tmp/day13.txt 
+awk -F: '!($1%($2*2-2)) {sum += $1*$2} END {print sum}' tmp/day13.txt 
+
+# to find out the modulus rules:
+#awk -F: '{len=$2*2-2; print len, (5*len-$1)%len}' tmp/day13.txt | sort -n
+
+# brute-force with shell takes about 10 seconds on my laptop :)
+awk -F: '{for(c=-$1;c<3999999;c+=$2*2-2)print(c);}' tmp/day13.txt \
+| grep -v '^-' \
+| sort -nu \
+| awk '$1 != last+1 && $1 {print last+1;} {last = $1}' \
+| head -1
