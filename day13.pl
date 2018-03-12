eb199fc499743ed944e4f4ec656b4ceba5b99763
Panu Kalliokoski <panu.kalliokoski@solita.fi>
2018-03-12 02:23:40 +0200

    enable all modulus rules in Prolog solution since they don't really slow the program down any

--- day13.pl
+++ day13.pl
@@ -7,17 +7,17 @@ natn(X) :- natn(Y), X is Y + 60060.
 % -> X = 60060 n + 6420, n e Z
 
 ok(N) :-
-	%0 is N mod 2,
-	%0 is N mod 4,
-	%0 is N mod 6,
-	%0 is N mod 10,
+	0 is N mod 2,
+	0 is N mod 4,
+	0 is N mod 6,
+	0 is N mod 10,
 	not(0 is N mod 8),
-	%8 is N mod 14,
+	8 is N mod 14,
 	not(12 is N mod 16),
 	not(0 is N mod 18),
 	not(12 is N mod 18),
-	%18 is N mod 22,
-	%24 is N mod 26,
+	18 is N mod 22,
+	24 is N mod 26,
 	not(4 is N mod 32),
 	not(12 is N mod 34),
 	not(22 is N mod 34).
