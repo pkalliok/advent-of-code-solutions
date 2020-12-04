59bcf7f5f8b3fc74e3b5c8bb0df85975268a8e6a
Panu Kalliokoski <panu.kalliokoski@sange.fi>
2020-12-04 13:48:41 +0200

    Generalise to N numbers

--- 2020/day1.py
+++ 2020/day1.py
@@ -1,12 +1,17 @@
+from itertools import product
 
 def file_to_numlist(filename):
     return [int(line) for line in open(filename)]
 
-def numbers_of_sum(total, nums):
-    return ((n1, n2) for n1 in nums for n2 in nums if n1+n2 == total)
+def cartesian_power(list, power):
+    return product(*([list]*power))
+
+def numbers_of_sum(total, nums, power):
+    return (comb for comb in cartesian_power(nums, power) if sum(comb) == total)
 
 if __name__ == '__main__':
     import sys
-    for n1, n2 in numbers_of_sum(2020, file_to_numlist(sys.argv[1])):
-        print(n1*n2)
+    numlist = file_to_numlist(sys.argv[1])
+    for n1, n2 in numbers_of_sum(2020, numlist, 2): print(n1*n2)
+    for n1, n2, n3 in numbers_of_sum(2020, numlist, 3): print(n1*n2*n3)
 
