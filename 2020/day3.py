595c460efe69fcfed1ea1c35e4eb6260a6d5b24c
Panu Kalliokoski <panu.kalliokoski@sange.fi>
2020-12-05 00:11:27 +0200

    Day3 part 1 solution

--- 2020/day3.py
+++ 2020/day3.py
@@ -5,6 +5,5 @@ def route(map, angle):
 if __name__ == '__main__':
     import sys
     mymap = [line.rstrip() for line in open(sys.argv[1])]
-    print(list(route(mymap,3)))
     print(sum(1 for obj in route(mymap, 3) if obj == "#"))
 
