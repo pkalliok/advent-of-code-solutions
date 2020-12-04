5a4044e3325c7bf35ce2c5b8d8cb20c78c780cc0
Panu Kalliokoski <panu.kalliokoski@sange.fi>
2020-12-05 00:27:33 +0200

    Day3 part 2 solution

--- 2020/day3.py
+++ 2020/day3.py
@@ -1,9 +1,20 @@
 
-def route(map, angle):
-    return (line[(i * angle) % len(line)] for i, line in enumerate(map))
+from math import log, exp
+
+def route(map, angle, gap):
+    return (line[(i // gap * angle) % len(line)]
+            for i, line in enumerate(map)
+            if i % gap == 0)
+
+def count_obstacles(map, angle, gap):
+    return sum(1 for obj in route(map, angle, gap) if obj == "#")
+
+directions = [(n, 1) for n in range(1,8,2)] + [(1,2)]
 
 if __name__ == '__main__':
     import sys
     mymap = [line.rstrip() for line in open(sys.argv[1])]
-    print(sum(1 for obj in route(mymap, 3) if obj == "#"))
+    print(count_obstacles(mymap, 3, 1))
+    print(round(exp(sum(log(count_obstacles(mymap, ang, gap))
+        for ang, gap in directions))))
 
