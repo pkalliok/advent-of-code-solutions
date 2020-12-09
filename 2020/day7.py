d25d697d6288ebea4145034b82b4a823a47b896f
Panu Kalliokoski <panu.kalliokoski@sange.fi>
2020-12-09 22:31:46 +0200

    Day7 part 2 solution

--- 2020/day7.py
+++ 2020/day7.py
@@ -4,17 +4,22 @@ from itertools import groupby
 import re
 
 rule_re = re.compile(r'(\w+ \w+) bags contain (.*)\.')
-content_re = re.compile(r'[0-9]+ (\w+ \w+) bags?')
+content_re = re.compile(r'([0-9]+) (\w+ \w+) bags?')
 
-def contain_pairs(rules):
-    return ((container, item)
+def contain_arcs(rules):
+    return ((container, amount, item)
             for container, content in
                 (rule_re.match(rule).groups() for rule in rules)
-            for item in content_re.findall(content))
+            for amount, item in content_re.findall(content))
 
-def rev_graph(pairs):
-    s = sorted((v, k) for k, v in pairs)
-    g = groupby(s, lambda p: p[0])
+def rev_graph(arcs):
+    return make_graph((v, k) for k, amount, v in arcs)
+
+def forw_graph(arcs):
+    return make_graph((k, (int(amount), v)) for k, amount, v in arcs)
+
+def make_graph(pairs):
+    g = groupby(sorted(pairs), lambda p: p[0])
     return {k: set(v for k, v in ps) for k, ps in g}
 
 def set_flatten(sset):
@@ -25,8 +30,14 @@ def transitive_closure(graph, start_set):
     if start_set == next_set: return next_set
     return transitive_closure(graph, next_set)
 
+def contained_bags_count(graph, bag):
+    return 1 + sum(am * contained_bags_count(graph, contained)
+            for am, contained in graph.get(bag, set()))
+
 if __name__ == '__main__':
     import sys
-    graph = rev_graph(contain_pairs(open(sys.argv[1])))
+    arcs = list(contain_arcs(open(sys.argv[1])))
+    graph = rev_graph(arcs)
     print(len(transitive_closure(graph, graph['shiny gold'])))
+    print(contained_bags_count(forw_graph(arcs), 'shiny gold')-1)
 
