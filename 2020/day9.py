059131661e912942882c9769986304d32cb72248
Panu Kalliokoski <panu.kalliokoski@sange.fi>
2020-12-10 00:09:58 +0200

    Day9 part 2 solution

--- 2020/day9.py
+++ 2020/day9.py
@@ -10,6 +10,25 @@ def block_invalid(block):
             for n1 in prevs for n2 in prevs
             if n1 + n2 == num if n1 != n2)
 
+def first_invalid_number(ls):
+    return next(filter(block_invalid, blocks(ls)))[0]
+
+def cont_range_totalling(total, ls):
+    start, end, running = 0, 1, ls[0]
+    while end < len(ls):
+        if end - start < 2 or running < total:
+            running += ls[end]
+            end += 1
+        elif running > total:
+            running -= ls[start]
+            start += 1
+        else: return ls[start:end]
+
 if __name__ == '__main__':
     import sys
-    print(next(filter(block_invalid, blocks(file_to_list(open(sys.argv[1]))))))
+    ls = file_to_list(open(sys.argv[1]))
+    inv = first_invalid_number(ls)
+    print(inv)
+    rng = cont_range_totalling(inv, ls)
+    print(min(rng) + max(rng))
+
