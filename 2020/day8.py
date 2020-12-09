ef973c4a50dcd6c3a6f7bb276fa4955695e1e702
Panu Kalliokoski <panu.kalliokoski@sange.fi>
2020-12-09 23:40:48 +0200

    Day8 part 2 solution

--- 2020/day8.py
+++ 2020/day8.py
@@ -16,6 +16,7 @@ def file_to_program(f):
 def exec_seq(prog, ip):
     while True:
         yield ip
+        if ip >= len(prog): return
         op, arg = prog[ip]
         ip += arg if op == 'jmp' else 1
 
@@ -29,7 +30,19 @@ def no_loop(seq):
 def acc_sum(prog, seq):
     return sum(arg for op, arg in (prog[ip] for ip in seq) if op == 'acc')
 
+def changed_program(prog, ip):
+    mapping = dict(jmp='nop', nop='jmp')
+    return [(mapping.get(op, op) if i==ip else op, arg)
+            for i, (op, arg) in enumerate(prog)]
+
+def find_fixed_seq(prog):
+    return next(filter(lambda seq: seq[-1] == len(prog),
+            (list(no_loop(exec_seq(changed_program(prog, ip), 0)))
+                for ip in range(len(prog)))))
+
 if __name__ == '__main__':
     import sys
     prog = file_to_program(open(sys.argv[1]))
     print(acc_sum(prog, no_loop(exec_seq(prog, 0))))
+    print(acc_sum(prog, find_fixed_seq(prog)[:-1]))
+
