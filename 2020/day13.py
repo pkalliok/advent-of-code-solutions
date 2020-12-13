bcbdd55800313fbc20d0d45f23720910ad75fb1e
Panu Kalliokoski <panu.kalliokoski@sange.fi>
2020-12-13 16:46:37 +0200

    Functionify day13 part 1 solution

--- 2020/day13.py
+++ 2020/day13.py
@@ -1,7 +1,16 @@
-import sys
-f = open(sys.argv[1])
-time = int(next(f))
-cycles = [int(cycle) for cycle in next(f).split(',') if cycle != 'x']
-possibilities = sorted((cycle - time % cycle, cycle) for cycle in cycles)
-waiting, lineid = possibilities[0]
-print(waiting * lineid)
+
+def params_from_file(f):
+    time = int(next(f))
+    moduli = [(int(cycle), pos) for pos, cycle in enumerate(next(f).split(','))
+            if cycle != 'x']
+    return time, moduli
+
+def shortest_waiting_time(time, moduli):
+    return min((cycle - time % cycle, cycle) for cycle, offset in moduli)
+
+if __name__ == '__main__':
+    import sys
+    time, moduli = params_from_file(open(sys.argv[1]))
+    waiting, line_id = shortest_waiting_time(time, moduli)
+    print(waiting * line_id)
+
